cd62657727ef3d536e23b6e58992c62d0a2bb461
Kyle Schechter <kyle.schechter@powerhrg.com>
2021-05-25 09:45:14 -0400

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+// Code your solutions in this fil
+function writeCards(array, event) {
+    let thankYou = []
+    for (let i = 0; i < array.length; i++) {
+        thankYou.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYou
+}
+
+function countDown(num) {
+    while (num > -1) {
+        console.log(num--)
+    }
+}
